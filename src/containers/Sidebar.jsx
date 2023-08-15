d930580fd0b2dd2cc20b93319d78eec5611ec684
samaksh <groversamaksh@gmail.com>
2023-08-16 01:29:32 +0530

    Search bar added

--- src/containers/Sidebar.jsx
+++ src/containers/Sidebar.jsx
@@ -1,11 +1,13 @@
 import { Stack } from "@mui/material";
 import { SidebarHeader } from "../components/SidebarHeader";
 import { ChatItems } from "./ChatItems";
+import { Searchbar } from "../components/Searchbar";
 
 export function Sidebar() {
   return (
     <Stack sx={{height:'100vh'}}>
       <SidebarHeader />
+      <Searchbar />
       <ChatItems/>
     </Stack>
   );
