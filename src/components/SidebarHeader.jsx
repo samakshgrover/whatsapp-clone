32b4e5a9c9dc8d5d5bd1ee302d6ef17eaef4ce12
samaksh <groversamaksh@gmail.com>
2023-08-16 01:28:20 +0530

    Sidebar header changes

--- src/components/SidebarHeader.jsx
+++ src/components/SidebarHeader.jsx
@@ -1,9 +1,23 @@
-import { AppBar, Toolbar } from "@mui/material";
+import { AppBar, Avatar, ButtonGroup, Toolbar } from "@mui/material";
+import avatar from '../assets/avatar1.jpg'
+import { CommunitiesIcon, MenuIcon, NewChatIcon, StatusIcon } from "./icons/icons";
 
 export function SidebarHeader(){
   return <>
-    <AppBar id="abcd" position="static" sx={{padding:0, margin:0}}>
-      <Toolbar sx={{padding:0, margin:0}}>Header</Toolbar>
+    <AppBar color="transparent" elevation={0} position="static" sx={{padding:0, margin:0, background: '#f0f2f5'}}>
+      <Toolbar sx={{padding:0, margin:0}}>
+        <Avatar src={avatar}></Avatar>
+        <HeaderIcons />
+      </Toolbar>
     </AppBar>
   </>
+}
+
+function HeaderIcons(){
+  return <ButtonGroup sx={{color:"#54656f" ,flexGrow:1, justifyContent:'flex-end', gap:"15px"}}>
+    <CommunitiesIcon />
+    <StatusIcon />
+    <NewChatIcon />
+    <MenuIcon />
+  </ButtonGroup>
 }
